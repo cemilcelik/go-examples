a3e002d9da4d3543838ab2bb503ff399979be1b3
cemilcelik <cemilcelik84@gmail.com>
2018-03-01 17:43:53 +0300

    Add range over channels example.

---
+++ range-over-channels/range-over-channels.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	queue := make(chan string, 3)
+
+	queue <- "one"
+	queue <- "two"
+	queue <- "three"
+	close(queue)
+
+	for elem := range queue {
+		fmt.Println(elem)
+	}
+}
